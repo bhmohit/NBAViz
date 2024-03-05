867720341bbae2d2b2ad2abc9c5ea1dbe0ad0877
Mohit Bhagchandani <bh.mohit29@gmail.com>
2024-03-05 00:02:31 -0800

    Finalize score component

--- nba-app/frontend/src/Live.jsx
+++ nba-app/frontend/src/Live.jsx
@@ -3,30 +3,30 @@ import "./Live.css";
 
 export default function Live() {
   return (
-    <Grid container spacing={0}>
-      <Grid item xs>
-        <div className="image">
-          <img
-            className="homeTeamImage"
-            src="https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg"
-          />
-        </div>
+    <div className="parent">
+      <Grid container spacing={0} className="main">
+        <Grid item xs>
+          <img src="https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg" />
+        </Grid>
+        <Grid item xs={6}>
+          <div className="middle">
+            <div className="status">Final</div>
+            <div className="data">
+              <span className="score">255</span>
+              <span className="score">255</span>
+            </div>
+            <div className="periods">
+              <span><b>1<sup>st</sup></b> 25-28</span>
+              <span><b>2<sup>st</sup></b> 25-28</span>
+              <span><b>3<sup>st</sup></b> 25-28</span>
+              <span><b>4<sup>st</sup></b> 25-28</span>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs>
+          <img src="https://cdn.nba.com/logos/nba/1610612752/global/L/logo.svg" />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <div className="middle">
-          <span className="score">255</span>
-          <span className="separator">-</span>
-          <span className="score">255</span>
-        </div>
-      </Grid>
-      <Grid item xs>
-        <div className="image">
-          <img
-            className="awayTeamImage"
-            src="https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg"
-          />
-        </div>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
